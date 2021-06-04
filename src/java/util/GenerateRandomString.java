18dbe5d02110b0f70ad5731e3a3b046e33d6b6ce
DatTTHE150411 <dattthe150411@fpt.edu.vn>
2021-06-04 23:02:11 +0700

    Add comment

--- src/java/util/GenerateRandomString.java
+++ src/java/util/GenerateRandomString.java
@@ -13,6 +13,11 @@ import java.util.Random;
  */
 public class GenerateRandomString {
 
+    /**
+     * Generate a random number of type String
+     * @param size: length of the number string
+     * @return the string of number
+     */
     public static String generateNumber(int size) {
         // These are the valid charecters use to random.
         String strValid = "0123456789";
@@ -26,6 +31,11 @@ public class GenerateRandomString {
         return mystring;
     }
 
+    /**
+     * Generate a random string
+     * @param size: length of the string
+     * @return the generated string
+     */
     public static String generateString(int size) {
         String str = "abcdefghijklmnopqrstuvwxyz";
         String str1 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
