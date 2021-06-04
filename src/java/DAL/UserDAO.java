f7810edf5b18fc6f193d3d4d659298e20349148b
DatTTHE150411 <dattthe150411@fpt.edu.vn>
2021-06-04 22:59:35 +0700

    Add comment, add get account by username method and change password method

--- src/java/DAL/UserDAO.java
+++ src/java/DAL/UserDAO.java
@@ -20,9 +20,17 @@ import java.util.List;
  */
 public class UserDAO extends BaseDAO<Account> {
 
+    
     PreparedStatement ps = null; //...
     ResultSet rs = null; //Nhận kết quả trả về
 
+    /**
+     * For login an account to the website
+     * @param user: username
+     * @param pass: password
+     * @return the account information of the account logging in,
+     * null if username or password does not exist
+     */
     public Account login(String user, String pass) {
         String query = "SELECT * FROM Users WHERE Username = ? AND Password = ?";
         try {
@@ -41,6 +49,13 @@ public class UserDAO extends BaseDAO<Account> {
         return null;
     }
 
+    /**
+     * Use for signup an account to the website
+     * @param user: username of the new account
+     * @param pass: password of the new account
+     * @param email: email of the new account
+     * @param activeCode: active code of the new account
+     */
     public void signUp(String user, String pass, String email, String activeCode) {
         String query = "INSERT INTO Users VALUES (?, ?, ?, ?, 0, 0, 2);";
         try {
@@ -54,6 +69,10 @@ public class UserDAO extends BaseDAO<Account> {
         }
     }
 
+    /**
+     * Get all account from database
+     * @return the list of account from the database
+     */
     public List<Account> getAllAccounts() {
         List<Account> list = new ArrayList<>();
         String query = "select * from Users";
@@ -72,6 +91,10 @@ public class UserDAO extends BaseDAO<Account> {
         return list;
     }
 
+    /**
+     * Delete an account from the database
+     * @param id: id of account to delete
+     */
     public void deleteAccount(String id) {
         String query = "delete from Cart where UserID = ?;\n"
                 + "delete from Cart where ProductID IN (select ProductID from Product where SellerID = ?);\n"
@@ -89,6 +112,15 @@ public class UserDAO extends BaseDAO<Account> {
         return;
     }
 
+    /**
+     * Update the information of an account
+     * @param id: id of account to update
+     * @param user: the updated username
+     * @param pass: the updated password
+     * @param email: the updated email
+     * @param isSell: the updated seller status of the account
+     * @param isAdmin : the updated admin status of the account
+     */
     public void editAccount(String id, String user, String pass, String email, String isSell, String isAdmin) {
         String query = "UPDATE Users\n"
                 + "SET Username = ?,\n"
@@ -110,6 +142,35 @@ public class UserDAO extends BaseDAO<Account> {
         }
     }
 
+    /**
+     * Update the password of a user
+     * @param id: of user
+     * @param newPassword: new password of that user
+     * @return true if update successfully
+     */
+    public boolean updatePassword(String id, String newPassword) {
+        Account toChange = getAccountByID(id);
+        String query = "UPDATE Users\n"
+                + "SET Password = ?\n"
+                + "WHERE UserID = ?";
+        try {
+            ps = connection.prepareStatement(query);
+            ps.setString(1, newPassword);
+            ps.setString(2, id);           
+            ps.executeUpdate();
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    /**
+     * Find an account by id
+     * @param id: id of account to find
+     * @return the found account, null if the account does not exist in the
+     * database
+     */
     public Account getAccountByID(String id) {
         String query = "select * from Users where UserID = ?";
         try {
@@ -127,6 +188,12 @@ public class UserDAO extends BaseDAO<Account> {
         return null;
     }
 
+    /**
+     * Find an account by email
+     * @param id: email of account to find
+     * @return the found account, null if the account does not exist in the
+     * database
+     */
     public Account getAccountByEmail(String userEmail) {
         String query = "select * from Users where email = ?";
         try {
@@ -144,15 +211,45 @@ public class UserDAO extends BaseDAO<Account> {
         return null;
     }
     
+    /**
+     * Find an account by username
+     * @param userName username of account to find
+     * @return the found account, null if the account does not exist in the
+     * database
+     */
+    public Account getAccountByUsername(String userName) {
+        String query = "select * from Users where Username = ?";
+        try {
+            ps = connection.prepareStatement(query);
+            ps.setString(1, userName);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                return new Account(rs.getInt(1), rs.getString(2),
+                        rs.getString(3), rs.getString(4),
+                        rs.getString(5), rs.getInt(6),
+                        rs.getInt(7), rs.getInt(8));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * Update status of an account
+     * @param id: id of account to update status
+     * @param status: the status to update to account
+     * @return true if update successful
+     */
     public boolean updateStatus(int id, int status) {
         String query = "Update Users set status = ? where UserID = ?";
         int check = 0;
 
-        try  {
+        try {
             ps = connection.prepareStatement(query);
             ps.setObject(2, id);
             ps.setObject(1, status);
-                                  
+
             check = ps.executeUpdate();
 
         } catch (SQLException ex) {
@@ -161,7 +258,10 @@ public class UserDAO extends BaseDAO<Account> {
         }
         return check > 0;
     }
-
+        
+    /**
+     * @return the number of account in the database 
+     */
     public int countAllAccount() {
         String query = "select count(*) from Users";
         try {
