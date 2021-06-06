360f542f3c3e7821c46fee1239938383f2551c81
nguyenthegiang <giangnthe153046@fpt.edu.vn>
2021-06-06 23:26:58 +0700

    Fix ListStatus.sql File

--- Database/ListStatus.sql
+++ Database/ListStatus.sql
@@ -1,5 +1,5 @@
 USE [ElectronicShop]
 GO
 
-INSERT INTO ProductStatus VALUES ( N'Available');
-INSERT INTO ProductStatus VALUES ( N'StopBusiness');
+INSERT INTO ProductStatus VALUES (1, N'Available');
+INSERT INTO ProductStatus VALUES (0, N'StopBusiness');
