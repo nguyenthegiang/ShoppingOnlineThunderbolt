fa4cb1680275c9a31ef1084b1b91011f451a3cd3
DatTTHE150411 <dattthe150411@fpt.edu.vn>
2021-06-16 23:30:24 +0700

    Add UserAddressDAO

---
+++ src/java/DAL/UserAddressDAO.java
@@ -0,0 +1,113 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DAL;
+
+import entity.UserAddress;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author TRANTATDAT
+ */
+public class UserAddressDAO extends BaseDAO<UserAddress> {
+
+    PreparedStatement ps = null;
+    ResultSet rs = null;
+
+    /**
+     * get all default ship address of user
+     *
+     * @return a list of ship address
+     */
+    public List<UserAddress> getAllUserAddress() {
+        List<UserAddress> list = new ArrayList<>();
+        String query = "SELECT * FROM UserAddress";
+        try {
+            ps = connection.prepareStatement(query);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                list.add(new UserAddress(
+                        rs.getInt("ID"),
+                        rs.getInt("UserID"),
+                        rs.getString("ShipName"),
+                        rs.getString("ShipAddress"),
+                        rs.getInt("ShipCityID"),
+                        rs.getString("PhoneNum")
+                ));
+            }
+            ps.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
+
+    /**
+     * get an address by id
+     *
+     * @param userAddressId
+     * @return an address
+     */
+    public UserAddress getAddressById(int userAddressId) {
+        String query = "SELECT * FROM UserAddress WHERE ID = ?";
+        UserAddress theAddress = new UserAddress();
+        try {
+            ps = connection.prepareStatement(query);
+            ps.setInt(1, userAddressId);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                theAddress = new UserAddress(
+                        rs.getInt("ID"),
+                        rs.getInt("UserID"),
+                        rs.getString("ShipName"),
+                        rs.getString("ShipAddress"),
+                        rs.getInt("ShipCityID"),
+                        rs.getString("PhoneNum")
+                );
+            }
+            ps.close();
+            return theAddress;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * get an address by user id
+     *
+     * @param userId
+     * @return an address
+     */
+    public UserAddress getAddressByUserId(int userId) {
+        String query = "SELECT * FROM UserAddress WHERE UserID = ?";
+        UserAddress theAddress = new UserAddress();
+        try {
+            ps = connection.prepareStatement(query);
+            ps.setInt(1, userId);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                theAddress = new UserAddress(
+                        rs.getInt("ID"),
+                        rs.getInt("UserID"),
+                        rs.getString("ShipName"),
+                        rs.getString("ShipAddress"),
+                        rs.getInt("ShipCityID"),
+                        rs.getString("PhoneNum")
+                );
+            }
+            ps.close();
+            return theAddress;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+          
+}
