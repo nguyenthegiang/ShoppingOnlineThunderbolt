d0f6a0b0c062f20f06977890c101e4b4566b8aff
thuanmit11 <73924438+thuanmit11@users.noreply.github.com>
2021-06-26 16:58:08 +0700

    adding notifications when approve an order from admin

--- src/java/control/ApproveOrder.java
+++ src/java/control/ApproveOrder.java
@@ -7,7 +7,9 @@ package control;
 
 import DAL.CartDAO;
 import DAL.InvoicesDAO;
+import DAL.NotificationDAO;
 import DAL.OrderDAO;
+import entity.Account;
 import entity.Order;
 import entity.OrderDetailAdmin;
 import java.io.IOException;
@@ -18,6 +20,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  *
@@ -38,11 +41,16 @@ public class ApproveOrder extends HttpServlet {
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-        int id = Integer.parseInt(request.getParameter("id"));
+        int orderId = Integer.parseInt(request.getParameter("orderId"));
+        int userId = Integer.parseInt(request.getParameter("userId"));
+        
+        
         try {
             OrderDAO orderDAO = new OrderDAO();
+            NotificationDAO notiDAO = new NotificationDAO();
             
-            orderDAO.packaging(id);
+            orderDAO.packaging(orderId);
+            notiDAO.approveOrderAdminNoti(userId, orderId);
             List<Order> orders = orderDAO.getAllOrder();
             
             
--- src/java/control/HomeControl.java
+++ src/java/control/HomeControl.java
@@ -36,12 +36,12 @@ public class HomeControl extends HttpServlet {
                 ProductDAO ProductDAO = new ProductDAO();
                 InforDAO InforDAO = new InforDAO();
                 CategoryDAO CategoryDAO = new CategoryDAO();
-           
+
                 List<Category> listC = CategoryDAO.getAllCategory(); //Get List Category
                 Product hot = ProductDAO.getHotProduct(); //Get First Product
                 Product favor = ProductDAO.getFavoriteProduct(); //Get Last Product
                 Information infor = InforDAO.getInfor(); //Get Information
-                
+
                 //Paging By CategoryID
                 String CategoryID = request.getParameter("CategoryID");
                 if (CategoryID == null) { //On Load: User hasn't choosen Category
@@ -148,28 +148,23 @@ public class HomeControl extends HttpServlet {
 
                 request.getRequestDispatcher("Home.jsp").forward(request, response);
             }
-        }
-    
-    catch (Exception e
-
-    
-        ) {
+        } catch (Exception e) {
             response.sendRedirect("Error.jsp");
+        }
+
     }
 
-}
-
 // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-/**
- * Handles the HTTP <code>GET</code> method.
- *
- * @param request servlet request
- * @param response servlet response
- * @throws ServletException if a servlet-specific error occurs
- * @throws IOException if an I/O error occurs
- */
-@Override
-        protected void doGet(HttpServletRequest request, HttpServletResponse response)
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         processRequest(request, response);
     }
@@ -183,7 +178,7 @@ public class HomeControl extends HttpServlet {
      * @throws IOException if an I/O error occurs
      */
     @Override
-        protected void doPost(HttpServletRequest request, HttpServletResponse response)
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         processRequest(request, response);
     }
@@ -194,7 +189,7 @@ public class HomeControl extends HttpServlet {
      * @return a String containing servlet description
      */
     @Override
-        public String getServletInfo() {
+    public String getServletInfo() {
         return "Short description";
     }// </editor-fold>
 
