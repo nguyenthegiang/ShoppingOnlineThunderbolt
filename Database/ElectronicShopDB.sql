977d57b95ddf84196598555ef28b2da72656c24b
huyenbnhe150346 <huyenbnhe150346@fpt.edu.vn>
2021-06-29 11:24:33 +0700

    Add database into Orders table

--- Database/ElectronicShopDB.sql
+++ Database/ElectronicShopDB.sql
@@ -306,7 +306,27 @@ INSERT INTO Orders VALUES (11, 2370000, 'hello', 3, '2021/06/23 21:23');
 INSERT INTO Orders VALUES (12, 8390000, 'black', 3, '2021/06/06 04:17');
 INSERT INTO Orders VALUES (3, 6720000, null, 3, '2021/06/14 12:36');
 INSERT INTO Orders VALUES (7, 17020000, 'hi', 3, '2021/06/12 10:18');
-
+INSERT INTO Orders VALUES (1, 7020000, null, 3, '2021/06/26 13:49');
+INSERT INTO Orders VALUES (2, 7020000, null, 3, '2021/06/23 21:23');
+INSERT INTO Orders VALUES (7, 7020000, null, 3, '2021/06/06 04:17');
+INSERT INTO Orders VALUES (9, 7020000, null, 3, '2021/06/16 12:36');
+INSERT INTO Orders VALUES (9, 7020000, null, 3, '2021/06/12 10:18');
+INSERT INTO Orders VALUES (11, 2000000, 'black', 3, '2021/05/23 21:23');
+INSERT INTO Orders VALUES (9, 250000, 'free shipping', 3, '2021/06/12 04:17');
+INSERT INTO Orders VALUES (6, 8020000, null, 3, '2021/06/26 12:36');
+INSERT INTO Orders VALUES (5, 1020000, null, 3, '2021/06/22 10:18');
+INSERT INTO Orders VALUES (1, 2370000, 'hello', 3, '2021/06/23 21:23');
+INSERT INTO Orders VALUES (2, 8390000, 'black', 3, '2021/06/06 04:17');
+INSERT INTO Orders VALUES (3, 6720000, null, 3, '2021/06/14 04:17');
+INSERT INTO Orders VALUES (7, 17020000, 'hi', 3, '2021/06/12 04:17');
+INSERT INTO Orders VALUES (7, 1020000, null, 3, '2021/06/22 10:18');
+INSERT INTO Orders VALUES (11, 2370000, 'hello', 3, '2021/06/23 21:23');
+INSERT INTO Orders VALUES (12, 8390000, 'black', 3, '2021/06/06 04:17');
+INSERT INTO Orders VALUES (3, 6720000, null, 3, '2021/06/14 12:36');
+INSERT INTO Orders VALUES (7, 17020000, 'hi', 3, '2021/06/12 10:18');
+INSERT INTO Orders VALUES (1, 7020000, null, 3, '2021/06/26 13:49');
+INSERT INTO Orders VALUES (2, 7020000, null, 3, '2021/06/23 21:23');
+INSERT INTO Orders VALUES (7, 7020000, null, 3, '2021/06/06 04:17');
 
 -------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
