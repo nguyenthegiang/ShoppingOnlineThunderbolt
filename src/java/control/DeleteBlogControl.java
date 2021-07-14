1a762b657850a501fe552f379e41ed1968fca3a3
ThongVipPro <thongpqhe150340@fpt.edu.vn>
2021-07-14 22:54:23 +0700

    Create Delete Blog Control

---
+++ src/java/control/DeleteBlogControl.java
@@ -0,0 +1,93 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package control;
+
+import DAL.BlogDAO;
+import DAL.ProductDAO;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author thong
+ */
+@WebServlet(name = "DeleteBlogControl", urlPatterns = {"/deleteBlog"})
+public class DeleteBlogControl extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        request.setCharacterEncoding("UTF-8");
+        try {
+            //Get the id of the product to be deleted
+            String id = request.getParameter("BlogID");
+            //Call delete() in ProductDAO
+            //ProductDAO dao = new ProductDAO();
+            BlogDAO dao = new BlogDAO();
+            dao.delete(id);
+            //Return to HomeControl page (link is /home) to reload data
+            //Don't need this command: request.getRequestDispatcher("home").forward(request,response); => Because I just need to switch pages, I don't need to bring data
+
+            response.sendRedirect("blogManager"); //Switch pages only
+            //request.getRequestDispatcher("home").forward(request,response); //Mang theo dữ liệu 
+        } catch (Exception e) {
+            response.sendRedirect("Error.jsp");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
